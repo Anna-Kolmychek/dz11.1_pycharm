af6294625c02681df00dbfaab5a5d4586f39a59a
AnnaKolmychek <a.kolmychek@gmail.com>
2023-04-23 21:38:31 +0300

    add func subtract_numbers

--- funcs.py
+++ funcs.py
@@ -2,4 +2,9 @@ def sum_numbers(num1: int, num2: int) -> int:
     return num1 + num2
 
 
+def subtract_numbers(num1: int, num2: int) -> int:
+    return num1 - num2
+
+
 print(sum_numbers(1, 1))
+print(subtract_numbers(1, 1))
